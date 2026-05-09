9afdcb06322014003123880a15b0a8f5a70e3c01
tomarmayankk <mayanktomarpc@gmail.com>
2026-05-09 17:29:10 +0530

    api for backend

--- admin/src/services/api.js
+++ admin/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5001/api"
+  baseURL: "https://property-listings-backend-jsxe.onrender.com/api"
 });
 
 // attach token automatically
